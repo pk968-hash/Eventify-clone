eb3ab0aa13c7196cad1e9cde61e0d8d4fc531e7a
pk968-hash <extrapiyush968@gmail.com>
2025-10-21 04:46:40 +0500

    ...

--- Server/src/server.js
+++ Server/src/server.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://eventify-snowy.vercel.app"],
     credentials: true,
   })
 );
